773d1c26a46372aa102fc76ecb1a364718554e06
Phil Thompson <phil@electricvisions.com>
2023-08-27 20:23:03 +0100

    Correct keymap for light

--- darko.nix
+++ darko.nix
@@ -13,9 +13,11 @@
   programs.light.enable = true;
   services.actkbd = {
     enable = true;
+    # nix-shell -p input-utils --run "sudo lsinput"
+    # nix-shell -p actkbd --run "sudo actkbd -n -s -d /dev/input/event1"
     bindings = [
-      { keys = [ 232 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
-      { keys = [ 233 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 5"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 5"; }
     ];
   };
 
