9a5dff7cc741d2db9bd1400c1d6eb61c7b49365c
Phil Thompson <phil@electricvisions.com>
2023-08-27 20:09:46 +0100

    Keymap for brightness

--- darko.nix
+++ darko.nix
@@ -11,6 +11,14 @@
 
   services.auto-cpufreq.enable = true;
   programs.light.enable = true;
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 232 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+      { keys = [ 233 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+    ];
+  };
+
 
   environment.systemPackages = with pkgs; [
   ];
