ae35bfff8e0d58733d63db26babc0431f08cc548
Phil Thompson <phil@electricvisions.com>
2023-08-28 14:17:27 +0100

    Improve file structure
    * So added files don't get missed from the build

--- copy_config.sh
+++ copy_config.sh
@@ -1,22 +1,15 @@
 machine=$1
-nixos_dir=$2
-dryrun=$3
+dryrun=$2
 
 source ./commands.sh
-source ./common.env
-source ./$machine.env
+source src/common.env
+source src/$machine.env
 
 lvm_partition=${ssd}p2
 
 STATE "COPY" "Copy config"
-RUN "sudo cp ./minimal.nix $nixos_dir"
-RUN "sudo cp ./common.nix $nixos_dir"
-RUN "sudo cp ./$machine.nix $nixos_dir/configuration.nix"
-RUN "sudo sed -i 's|USER_NAME|$user_name|' $nixos_dir/minimal.nix"
-RUN "sudo sed -i 's|USER_FULLNAME|$user_full_name|' $nixos_dir/minimal.nix"
-RUN "sudo sed -i 's|USER_PASSWORD|$user_password|' $nixos_dir/minimal.nix"
-RUN "sudo sed -i 's|LVM_PARTITION|$lvm_partition|' $nixos_dir/minimal.nix"
-
-STATE "NVIM" "Copy Neovim config"
-RUN "sudo rm -rf $nixos_dir/neovim"
-RUN "sudo cp -r neovim $nixos_dir"
+RUN "cp src/minimal_template.nix src/minimal.nix"
+RUN "sed -i 's|USER_NAME|$user_name|' src/minimal.nix"
+RUN "sed -i 's|USER_FULLNAME|$user_full_name|' src/minimal.nix"
+RUN "sed -i 's|USER_PASSWORD|$user_password|' src/minimal.nix"
+RUN "sed -i 's|LVM_PARTITION|$lvm_partition|' src/minimal.nix"
