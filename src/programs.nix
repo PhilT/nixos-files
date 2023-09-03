0878d49c0b6a73a34674a5efebcdc4a8cc364447
Phil Thompson <phil@electricvisions.com>
2023-09-03 11:25:46 +0100

    Fix Chromium settings

--- src/programs.nix
+++ src/programs.nix
@@ -17,9 +17,11 @@
       extensions = [
         "cgbcahbpdhpcegmbfconppldiemgcoii" # ublock origin
         "cdkhedhmncjnpabolpjceohohlefegak" # Startpage privacy protection
+        "paoafodbgcjnmijjepmpgnlhnogaahme" # Material Dark theme
       ];
+      defaultSearchProviderEnabled = true;
       defaultSearchProviderSearchURL = "https://www.startpage.com/sp/search?query={searchTerms}&cat=web&pl=chrome";
-      homepageLocation = "https://startpage.com";
+      homepageLocation = "https://www.startpage.com";
     };
   };
 
