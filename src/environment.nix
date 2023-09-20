706ce9a5d3c785425eed79c00415c1691300b5ca
Phil Thompson <phil@electricvisions.com>
2023-09-20 22:26:36 +0100

    Add ranger and move some xorg config

--- src/environment.nix
+++ src/environment.nix
@@ -1,33 +1,41 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  systemd.tmpfiles.rules = [
-    "d /data - phil users"
-    "d /data/downloads - phil users"
-  ];
+  options.xorg.xdgConfigHome = lib.mkOption {
+    type = lib.types.str;
+    default = "/home/phil/.config";
+    description = "Standard XDG_CONFIG_HOME";
+  };
 
-  environment = {
-    sessionVariables = rec {
-      # Duplicated in ./initialize
-      DATA = "/data";
-      CODE = "${DATA}/code";
-      SRC = "${CODE}/nixos-files";
-      TXT  = "${DATA}/txt";
-      CDPATH   = "${CODE}:${DATA}";
-      DOTNET_CLI_TELEMETRY_OPTOUT = "true";
-      FZF_DEFAULT_COMMAND = "rg --files --no-ignore-vcs --hidden --ignore-file ~/.ignore";
-      HISTCONTROL = "ignorespace:erasedups";   # Don't add commands starting with space, remove previous occurrances of command
-      HISTFILESIZE = "";                       # Unlimited history
-      HISTSIZE = "";                           # Unlimited history
-      XDG_CONFIG_HOME = "$HOME/.config";
-      GTK_THEME = "Adwaita:dark";
-    };
+  config = {
+    systemd.tmpfiles.rules = [
+      "d /data - phil users"
+      "d /data/downloads - phil users"
+    ];
 
-    etc = {
-      "bashrc.local".source = ../secrets/bashrc.local;
-      "xdg/nvim/colors/greyscale.vim".source = ../neovim/colors/greyscale.vim;
-      "gitignore".source = ../dotfiles/gitignore;
-      "ignore".source = ../dotfiles/ignore;
+    environment = {
+      sessionVariables = rec {
+        # Duplicated in ./initialize
+        DATA = "/data";
+        CODE = "${DATA}/code";
+        SRC = "${CODE}/nixos-files";
+        TXT  = "${DATA}/txt";
+        CDPATH   = "${CODE}:${DATA}";
+        DOTNET_CLI_TELEMETRY_OPTOUT = "true";
+        FZF_DEFAULT_COMMAND = "rg --files --no-ignore-vcs --hidden --ignore-file ~/.ignore";
+        HISTCONTROL = "ignorespace:erasedups";   # Don't add commands starting with space, remove previous occurrances of command
+        HISTFILESIZE = "";                       # Unlimited history
+        HISTSIZE = "";                           # Unlimited history
+        XDG_CONFIG_HOME = config.xorg.xdgConfigHome;
+        GTK_THEME = "Adwaita:dark";  # TODO: Move to xserver config?
+      };
+
+      etc = {
+        "bashrc.local".source = ../secrets/bashrc.local;
+        "xdg/nvim/colors/greyscale.vim".source = ../neovim/colors/greyscale.vim;
+        "gitignore".source = ../dotfiles/gitignore;
+        "ignore".source = ../dotfiles/ignore;
+      };
     };
   };
 }
--- src/filemanager.nix
+++ src/filemanager.nix
@@ -5,8 +5,10 @@
     systemPackages = with pkgs; [
       lxde.lxmenu-data      # List apps to run in PCManFM
       pcmanfm
+      ranger                # Terminal file manager
     ];
 
+    # TODO: How would this be useful for ranger?
     etc."config/gtk-3.0/bookmarks" = {
       mode = "444";
       text = ''
@@ -21,4 +23,8 @@
       '';
     };
   };
+
+  systemd.tmpfiles.rules = [
+    "L+ ${config.xorg.xdgConfigHome}/gtk-3.0/bookmarks - - - - /etc/config/gtk-3.0/bookmarks"
+  ];
 }
