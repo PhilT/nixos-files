247974f3ba47ba88e76d092c874a805c029b5592
Phil Thompson <34678+PhilT@users.noreply.github.com>
2024-01-08 22:00:59 +0000

    Update hid-fanatecff

--- src/hid-fanatecff/default.nix
+++ src/hid-fanatecff/default.nix
@@ -10,14 +10,14 @@ let moduledir = "lib/modules/${kernel.version}/kernel/drivers/hid";
 in
 stdenv.mkDerivation rec {
   pname = "hid-fanatecff";
-  version = "0.0.2";
+  version = "0.0.3";
   name = "hid-fanatecff-${version}-${kernel.version}";
 
   src = fetchFromGitHub {
     owner = "gotzl";
     repo = "hid-fanatecff";
     rev = "next";
-    sha256 = "FZc69a2joiCi8pPxeJxhORaqa/ABRecPqDhDCc2GwL0=";
+    sha256 = "U5lWGQuTdsa/gshCApnA/JVj7kPXPtQYTy+kMdbqycw=";
   };
 
   hardeningDisable = [ "pic" "format" ];
@@ -27,13 +27,13 @@ stdenv.mkDerivation rec {
     mkdir -p $out/lib/udev/rules.d
     mkdir -p $out/${moduledir}
     substituteInPlace Makefile --replace "/etc/udev/rules.d" "$out/lib/udev/rules.d"
-    substituteInPlace fanatec.rules --replace "/usr/bin/evdev-joystick" "${linuxConsoleTools}/bin/evdev-joystick" --replace "GROUP:=\"plugdev\"" "TAG+=\"uaccess\""
+    substituteInPlace fanatec.rules --replace "/usr/bin/evdev-joystick" "${linuxConsoleTools}/bin/evdev-joystick" --replace "/bin/sh" "${stdenv.shell}"
     sed -i '/depmod/d' Makefile
   '';
 
   makeFlags = [
     "KVERSION=${kernel.modDirVersion}"
-    "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "KERNEL_SRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
     "MODULEDIR=$(out)/${moduledir}"
   ];
 }
