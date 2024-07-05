eb9bbc158edd544da1d31c7a7c882bc24d3c35cb
pthompson3 <34678+PhilT@users.noreply.github.com>
2024-07-05 17:44:32 +0100

    Add support scripts for .persistent_history

--- src/programs.nix
+++ src/programs.nix
@@ -13,10 +13,15 @@
     # Starship - Highly configurable shell prompt
     starship.enable = true;
 
+    # Log all history to a separate history file after every command.
+    # CTRL+R stays clean for the current shell but you can always refer
+    # to ~/.persistent_history if you need a command from another shell.
+    # Use hgrep (below) to search this history file.
     bash.promptInit = ''
       log_bash_persistent_history() {
-        local cmd=$(history 1)
-        if [ "$cmd" != "$PERSISTENT_HISTORY_LAST" ]; then
+        [[ $(history 1) =~ ^\ *[0-9]+\ +(.*)$ ]]
+        local cmd="''${BASH_REMATCH[1]}"
+        if [[ "$cmd" != "$PERSISTENT_HISTORY_LAST" && "$cmd" != "hcat" && "$cmd" != "hcompact" ]]; then
           echo "$cmd" >> ~/.persistent_history
           export PERSISTENT_HISTORY_LAST="$cmd"
         fi
@@ -75,6 +80,11 @@
         find . -type d -print0 | xargs -0 chmod 755
         find . -type f -print0 | xargs -0 chmod 644
       '')
+
+      # Search in ~/.persistent_history file (see above)
+      (writeShellScriptBin "hgrep" "grep $@ ${config.userHome}/.persistent_history")
+      (writeShellScriptBin "hcat" "cat ${config.userHome}/.persistent_history")
+      (writeShellScriptBin "hcompact" "awk -i inplace '!seen[$0]++' ${config.userHome}/.persistent_history") # Runs on Hyprland start
 
       # TODO: Probably don't need this anymore
       (writeShellScriptBin "slk" ''
