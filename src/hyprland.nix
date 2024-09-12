7951f12a8660f4145ec5c2061c1265145fe6ca51
pthompson3 <34678+PhilT@users.noreply.github.com>
2024-09-12 08:38:05 +0100

    Turn off splash text in Hyprland + reformat

--- src/hyprland.nix
+++ src/hyprland.nix
@@ -97,47 +97,47 @@ in with colors; {
           }
 
           decoration {
-              rounding = 3
+            rounding = 3
 
-              # Change transparency of focused and unfocused windows
-              active_opacity = 1.0
-              inactive_opacity = 1.0
+            # Change transparency of focused and unfocused windows
+            active_opacity = 1.0
+            inactive_opacity = 1.0
 
-              drop_shadow = true # true
-              shadow_range = 4
-              shadow_render_power = 3
-              col.shadow = rgba(1a1a1aee)
+            drop_shadow = true # true
+            shadow_range = 4
+            shadow_render_power = 3
+            col.shadow = rgba(1a1a1aee)
 
-              # https://wiki.hyprland.org/Configuring/Variables/#blur
-              blur {
-                  enabled = true # true
-                  size = 3
-                  passes = 1
+            # https://wiki.hyprland.org/Configuring/Variables/#blur
+            blur {
+              enabled = true # true
+              size = 3
+              passes = 1
 
-                  vibrancy = 0.1696
-              }
+              vibrancy = 0.1696
+            }
           }
 
           # https://wiki.hyprland.org/Configuring/Variables/#animations
           animations {
-              enabled = true # true
+            enabled = true # true
 
-              # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+            # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-              bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+            bezier = myBezier, 0.05, 0.9, 0.1, 1.05
 
-              animation = windows, 1, 7, myBezier
-              animation = windowsOut, 1, 7, default, popin 80%
-              animation = border, 1, 5, default
-              animation = borderangle, 1, 8, default
-              animation = fade, 1, 7, default
-              animation = workspaces, 1, 6, default
+            animation = windows, 1, 7, myBezier
+            animation = windowsOut, 1, 7, default, popin 80%
+            animation = border, 1, 5, default
+            animation = borderangle, 1, 8, default
+            animation = fade, 1, 7, default
+            animation = workspaces, 1, 6, default
           }
 
           # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
           dwindle {
-              pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-              preserve_split = true # You probably want this
+            pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+            preserve_split = true # You probably want this
           }
 
           # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
@@ -147,8 +147,9 @@ in with colors; {
 
           # https://wiki.hyprland.org/Configuring/Variables/#misc
           misc {
-              force_default_wallpaper = -1 # Set to 0 or 1 to disable the anime mascot wallpapers
-              disable_hyprland_logo = false # If true disables the random hyprland logo / anime girl background. :(
+            force_default_wallpaper = -1 # Set to 0 or 1 to disable the anime mascot wallpapers
+            disable_hyprland_logo = false # If true disables the random hyprland logo / anime girl background. :(
+            disable_splash_rendering = true # Disable splash text
           }
 
 
@@ -158,38 +159,38 @@ in with colors; {
 
           # https://wiki.hyprland.org/Configuring/Variables/#input
           input {
-              kb_layout = gb
-              kb_variant =
-              kb_model =
-              kb_options = ctrl:nocaps
-              kb_rules =
+            kb_layout = gb
+            kb_variant =
+            kb_model =
+            kb_options = ctrl:nocaps
+            kb_rules =
 
-              follow_mouse = 1
+            follow_mouse = 1
 
-              sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+            sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
 
-              touchpad {
-                  natural_scroll = true
-              }
+            touchpad {
+              natural_scroll = true
+            }
           }
 
           # https://wiki.hyprland.org/Configuring/Variables/#gestures
           gestures {
-              workspace_swipe = false
+            workspace_swipe = false
           }
 
           # Example per-device config
           # See https://wiki.hyprland.org/Configuring/Keywords/#per-device-input-configs for more
           device {
-              name = epic-mouse-v1
-              sensitivity = -0.5
+            name = epic-mouse-v1
+            sensitivity = -0.5
           }
 
           device {
-              name = ydotoold-virtual-device
-              kb_layout = us
-              kb_variant =
-              kb_options =
+            name = ydotoold-virtual-device
+            kb_layout = us
+            kb_variant =
+            kb_options =
           }
 
           ###################
